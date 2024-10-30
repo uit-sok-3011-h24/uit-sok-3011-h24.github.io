14f1375a1f539cc68f16cd1b294ef72219a23586
espensirnes <espen.sirnes@uit.no>
2024-10-30 13:12:21 +0100

    new fin4

--- finans/functions.py
+++ finans/functions.py
@@ -1,21 +1,21 @@
-import numpy as np
-from scipy.cluster.hierarchy import linkage, fcluster
-import pandas as pd
-import decomposition
-
-
-def get_matrix(df, field):
-	"""Converts the df to a matrix df that can 
-	be used to calculate the covariance matrix"""
+import nbformat
+executed_notebooks = []
+d = {}
+def calc_notebook(notebook_path, ques):
+	if notebook_path in executed_notebooks:
+		return d
+	executed_notebooks.append(notebook_path)
 	
-	df['Date'] = pd.to_datetime(df['Date'])
-	df_unique = df.drop_duplicates(subset=['Date', 'ISIN'])
-	pivot_df = df_unique.pivot(index='Date', columns='ISIN', values=field)
-	pivot_df = pivot_df.dropna()
-	df_weekly = pivot_df.resample('W').sum()*52 #annualized
+	with open(notebook_path, encoding="utf-8") as f:
+		notebook = nbformat.read(f, as_version=4)
+	for c in  notebook.cells:
+		if c.cell_type == "code":
+			if sum(q in c.source for q in ques):
+				exec(c.source, globals(), d)
+	return d
 
-	return np.array(df_weekly)
 
-X = get_matrix(df, 'lnDeltaP')
-cov_matrix = np.cov(X, rowvar=False)
-means = np.mean(X, axis=0).reshape((X.shape[1],1))
+
+calc_notebook("3-lecture_optport.ipynb", 
+			  ["read_pickle","get_matrix", "cov_matrix", "A =", 
+	  		   "portfolio_front", "ax.plot"])
